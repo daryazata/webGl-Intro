a98ba73c5630e7b42d8810b5a5943f8a7e7185e5
darya zata <darya.zata@gmail.com>
2024-07-07 17:55:28 +0200

    webGl-2: add colors and animation

--- init-buffers.js
+++ init-buffers.js
@@ -1,8 +1,10 @@
 function initBuffers(gl) {
   const positionBuffer = initPositionBuffer(gl);
+  const colorBuffer = initColorBuffer(gl);
 
   return {
     position: positionBuffer,
+    color: colorBuffer,
   };
 }
 
@@ -15,7 +17,7 @@ function initPositionBuffer(gl) {
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
   // Now create an array of positions for the square.
-  const positions = [1.0, 1.0, -2.0, 2.0, 1.0, -2.0, -3.0, -1.0];
+  const positions = [2.0, 2.0, -2.0, 2.0, 2.0, -2.0, -2.0, -2.0];
 
   // Now pass the list of positions into WebGL to build the
   // shape. We do this by creating a Float32Array from the
@@ -26,3 +28,30 @@ function initPositionBuffer(gl) {
 }
 
 export { initBuffers };
+
+function initColorBuffer(gl) {
+  const colors = [
+    0.0,
+    1.0,
+    1.0,
+    1.0, // Cyan
+    1.0,
+    0.0,
+    0.0,
+    1.0, // red
+    0.0,
+    0.0,
+    1.0,
+    1.0, // green
+    1.0,
+    1.0,
+    0.0,
+    1.0, // yellow
+  ];
+
+  const colorBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+
+  return colorBuffer;
+}
